97c4a004193d4aa5408b7ec890fbbfea59f27353
Fazilio <74589570+Fazilio@users.noreply.github.com>
2020-11-18 11:38:54 +0300

    FMI_TASK_03

---
+++ FMI_TASK_03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FMI_TASK_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "microsoft";
+            string userPassword;
+            int attempt = 3;
+
+            Console.WriteLine("Введите пароль: ");
+
+            for (attempt = 3; attempt >= 1; --attempt)
+            {
+                userPassword = Console.ReadLine();
+                if (userPassword == password)
+                {
+                    Console.WriteLine("\nПоставьте 5 :)");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Повторите попытку: ");
+                }
+            }
+        }
+    }
+}
